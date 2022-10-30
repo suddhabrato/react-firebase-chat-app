b09f78e0e229d408361854d4b40106f216afd19d
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2022-10-30 22:58:46 +0530

    Added ChatRoom and ChatMessage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
@@ -52,6 +52,56 @@ function SignOut() {
         Sign Out
       </button>
     )
+  );
+}
+
+function ChatRoom() {
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+  const [messages] = useCollectionData(query, { idField: "id" });
+  const [formValue, setFormValue] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const { uid, photoURL } = auth.currentUser;
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+    setFormValue("");
+  };
+
+  return (
+    <div>
+      <div>
+        {messages &&
+          messages.map((message, ind) => (
+            <ChatMessage key={ind} {...message} />
+          ))}
+      </div>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => {
+            setFormValue(e.target.value);
+          }}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+}
+
+function ChatMessage({ text, uid, photoURL }) {
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} />
+      <p>{text}</p>
+    </div>
   );
 }
 
