b8649da60c9c858875676092601715be14b48634
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2022-10-30 23:14:32 +0530

    Added basic css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
@@ -38,13 +38,18 @@ function SignIn() {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   };
-  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+  return (
+    <button className="sign-in" onClick={signInWithGoogle}>
+      Sign in with Google
+    </button>
+  );
 }
 
 function SignOut() {
   return (
     auth.currentUser && (
       <button
+        className="sign-out"
         onClick={() => {
           auth.signOut();
         }}
@@ -56,6 +61,7 @@ function SignOut() {
 }
 
 function ChatRoom() {
+  const dummy = useRef();
   const messagesRef = firestore.collection("messages");
   const query = messagesRef.orderBy("createdAt").limit(25);
   const [messages] = useCollectionData(query, { idField: "id" });
@@ -71,26 +77,31 @@ function ChatRoom() {
       photoURL,
     });
     setFormValue("");
+    dummy.current.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
-    <div>
-      <div>
+    <>
+      <main>
         {messages &&
           messages.map((message, ind) => (
             <ChatMessage key={ind} {...message} />
           ))}
-      </div>
+        <span ref={dummy}></span>
+      </main>
       <form onSubmit={sendMessage}>
         <input
           value={formValue}
           onChange={(e) => {
             setFormValue(e.target.value);
           }}
+          placeholder="say something nice"
         />
-        <button type="submit">Send</button>
+        <button type="submit" disabled={!formValue}>
+          Send
+        </button>
       </form>
-    </div>
+    </>
   );
 }
 
@@ -99,7 +110,11 @@ function ChatMessage({ text, uid, photoURL }) {
 
   return (
     <div className={`message ${messageClass}`}>
-      <img src={photoURL} />
+      <img
+        src={
+          photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
+        }
+      />
       <p>{text}</p>
     </div>
   );
